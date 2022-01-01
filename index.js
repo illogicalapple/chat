086f72d2e02b8b13ddeb6bd01e52350463837672
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-01 14:45:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var ably = new Ably.Realtime("p7omrg.j5cIZw:PQIh52fSUKb_sRfyiBGXGhNzg-JQE0Fjp2PYMrx56EY");
 var channel = ably.channels.get("general");
+function escape(stuf) {
+	return stuf.split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;")
+}
 channel.subscribe("stuf", function(message) {
-	alert(message.data);
+	let thing = document.createElement("div");
+	let user = document.createElement("strong");
+	let content = document.createElement("span");
+	user.innerText = "@" + escape(message.split(":")[0]) + ":&nbsp;";
+	content.innerText = escape(message.split(":")[1]);
+	thing.appendChild(user);
+	thing.appendChild(content);
+	document.querySelector("div.messages").appendChild(thing);
 });
