65141bd724adb9b5a8acd4908adb9d0813ba0d6f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-01 15:07:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var ably = new Ably.Realtime("p7omrg.j5cIZw:PQIh52fSUKb_sRfyiBGXGhNzg-JQE0Fjp2PYMrx56EY");
 var channel = ably.channels.get("general");
 function escape(stuf) {
-	return String(stuf.data).split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;")
+	return String(stuf).split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;")
 }
 channel.subscribe("stuf", function(message) {
 	console.log(message);
 	let thing = document.createElement("div");
 	let user = document.createElement("strong");
 	let content = document.createElement("span");
-	user.innerText = "@" + escape(message.split(":")[0]) + ":&nbsp;";
-	content.innerText = escape(message.split(":")[1]);
+	user.innerText = "@" + escape(message.data.split(":")[0]) + ":&nbsp;";
+	content.innerText = escape(message.data.split(":")[1]);
 	thing.appendChild(user);
 	thing.appendChild(content);
 	document.querySelector("div.messages").appendChild(thing);
