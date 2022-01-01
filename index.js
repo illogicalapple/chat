146c9e6d5c749ac504e247fbc192526e76a8166a
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-01-01 15:49:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 var ably = new Ably.Realtime("p7omrg.j5cIZw:PQIh52fSUKb_sRfyiBGXGhNzg-JQE0Fjp2PYMrx56EY");
 var channel = ably.channels.get("general");
+var name = "Person";
 function escape(stuf) {
-	return String(stuf).split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;")
+	return String(stuf).split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;").split("").join(":");
 }
 channel.subscribe("stuf", function(message) {
 	console.log(message);
 	let thing = document.createElement("div");
 	let user = document.createElement("strong");
 	let content = document.createElement("span");
-	user.innerText = "@" + escape(message.data.split(":")[0]) + ":&nbsp;";
+	user.innerText = "@" + escape(message.data.split(":")[0]) + ": ";
 	content.innerText = escape(message.data.split(":")[1]);
 	thing.appendChild(user);
 	thing.appendChild(content);
 	document.querySelector("div.messages").appendChild(thing);
 });
+function enter(event) {
+	if(event.key == "Enter" && document.querySelector("input").value !== "") {
+		channel.publish("stuf", escape(name).replace(":", "") + ":" + escape(document.querySelector("input").value).replace(":", ""));
+	}
+}
+document.querySelector("main input").addEventListener("keydown", enter);
+document.querySelector("main button").addEventListener("click", enter);
